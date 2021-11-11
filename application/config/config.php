1fa2d35bea967cc44b88f2abad367f2ef70a2de7
garciaisnt <cgarcia9911@hotmail.com>
2021-11-10 20:58:25 -0600

    Usuarios crud

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sistema_namae/';
+$config['base_url'] = 'http://sistema_de_registro_academico_web.test/';
+//comentario
 
 /*
 |--------------------------------------------------------------------------
