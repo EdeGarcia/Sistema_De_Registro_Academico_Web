1c3d5b3f7463ef88570d811b1c11533286866cfc
garciaisnt <cgarcia9911@hotmail.com>
2021-11-15 00:46:12 -0600

    mostrar nombre en lugar de id en la lista

--- application/views/UsuariosView.php
+++ application/views/UsuariosView.php
@@ -1,4 +1,4 @@
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col-md-12 mt-5">
             <h1 class="text-center">Usuarios</h1>
@@ -9,20 +9,18 @@
     <div class="row">
         <div class="col-md-12 mt-2">
 
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_add"  onclick="modificarTituloModal('Agregar usuario');">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_add" onclick="modificarTituloModal('Agregar usuario');">
                 Agregar
             </button>
 
-            <!-- Modal para agregar -->
-            <div class="modal fade" id="modal_add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <!-- Modal para agregar/editar -->
+            <div class="modal fade" id="modal_add" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered">
                     <div class="modal-content">
                         <div class="modal-header">
 
                             <h5 class="modal-title" id="tituloModal">Agregar/modificar</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                            
 
                         </div>
                         <div class="modal-body">
@@ -34,9 +32,19 @@
                                 </div>
 
                                 <!-- Contraseña -->
-                                <div class="form-group">
-                                    <label for="">Contraseña</label>
+
+                                <label for="">Contraseña</label>
+                                <div class="input-group mb-3">
+
                                     <input type="password" class="form-control" name="ins_contrasena" required>
+                                    <div id="ocultarClave">
+                                        <div class="input-group-prepend">
+                                            <div class="input-group-text">
+                                                <input type="checkbox" id="btnCambiarClave" onclick="manipularInputClave()">
+                                                <span>Cambiar</span>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
 
                                 <!-- Rol -->
@@ -50,7 +58,6 @@
                                         <option value="Maestro">Maestro</option>
                                     </select>
                                 </div>
-
 
                                 <!-- IdEmpleado -->
                                 <div class="form-group">
@@ -67,17 +74,15 @@
 
                         <div class="modal-footer">
 
-                            <button type="button" class="btn btn-primary" onclick="insertar()">Guardar</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                            <button id ="btnInsertar" type="button" class="btn btn-primary" onclick="insertar()">Guardar</button>
+                            <button id ="btnEditar" type="button" class="btn btn-primary" onclick="editar()">Guardar cambios</button>
+
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal" onclick="limpiarModal();">Cancelar</button>
                         </div>
                         </form>
                     </div>
                 </div>
             </div>
-
-
-
-
 
             <!-- Modal seleccionar Empleado -->
             <div class="modal fade" id="SeleccionarEmpleado" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-hidden="true">
@@ -107,44 +112,6 @@
                     </div>
                 </div>
             </div>
-
-
-
-
-
-            <!-- Modal para editar -->
-            <div class="modal fade" id="modal_edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-
-                            <h5 class="modal-title" id="exampleModalLabel">Actualizar Materia</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-
-                        </div>
-                        <div class="modal-body">
-                            <form action="#" method="post" id="form_edit">
-                                <input type="hidden" name="edit_id">
-                                <!-- Usuario -->
-                                <div class="form-group">
-                                    <label for="">Usuario</label>
-                                    <input type="text" name="edit_usuario" class="form-control">
-                                </div>
-                            </form>
-                            <a href="#modal-1" data-toggle="modal" data-dismiss="modal">Previous</a>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" id="update">Guardar</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
         </div>
     </div>
 
@@ -173,26 +140,52 @@
 </div>
 
 <script>
-
     const modificarTituloModal = (titulo) => {
         $("#tituloModal").text(titulo);
+
+        if (titulo === 'Agregar usuario') {
+            $("#ocultarClave").hide();
+            $("#btnEditar").hide();
+            $("#btnInsertar").show();
+            $("[name='ins_contrasena']").prop("disabled", false);
+        } else {
+            $("#btnInsertar").hide();
+            $("#btnEditar").show();
+
+            $("[name='ins_contrasena']").prop("disabled", true);
+            $("#ocultarClave").show();
+        }
     }
 
+    const limpiarModal = () => {
+        $("#ins_idEmpleado").val('');
+        $('#form_add').trigger("reset");
+    }
+
+    const manipularInputClave = () => {
+        if ($("#btnCambiarClave").is(":checked")) {
+            $("[name='ins_contrasena']").prop("disabled", false);
+        } else {
+            $("[name='ins_contrasena']").prop("disabled", true);
+        }
+    }
+   
     const cargarUsuarios = () => {
         $.get({
             url: 'cargarUsuarios',
             success: function(result) {
                 let lista = "";
-                result.forEach(usuario => {
+                result.forEach(usuario => {                   
+                    
                     lista += `
                     <tr>
                         <td>${usuario.IDUsuario}</td>
                         <td>${usuario.Usuario}</td>
                         <td>${usuario.Rol}</td>
-                        <td>${usuario.IDEmpleado}</td>
+                        <td>${nombreEmpleadoPorId(usuario.IDEmpleado)}</td>
                         <td>
-                            <a href="#" id="edit" onclick="cargarEmpleado('${usuario.IDUsuario}')" class="btn btn-sm btn-outline-success"><i class="fas fa-edit"></i></a>                                       
-                            <a href="#" id="del" onclick="cargarEmpleado('${usuario.IDUsuario}')" class="btn btn-sm btn-outline-danger"><i class="fas fa-trash"></i></a>
+                            <a href="#" id="edit" onclick="cargarUsuario('${usuario.IDUsuario}')" class="btn btn-sm btn-outline-success"><i class="fas fa-edit"></i></a>                                       
+                            <a href="#" id="del" onclick="cargarUsuario('${usuario.IDUsuario}')" class="btn btn-sm btn-outline-danger"><i class="fas fa-trash"></i></a>
                         </td>
                     </tr>`;
                 });
@@ -222,9 +215,25 @@
                 $('#tbodyEmpleados').html(lista);
             }
         });
+    }   
+
+    const nombreEmpleadoPorId = (id) => {
+        let nombre;
+        $.get({
+            url: 'cargarEmpleado',
+            async: false,
+            dataType: "json",
+            data: {
+                id: id
+            },
+            success: function(empleado) {               
+                nombre = empleado.Nombres + ' ' + empleado.Apellidos;              
+            }
+        });
+        return nombre;
     }
 
-    const cargarEmpleado = (id) => {
+    const cargarUsuario = (id) => {
         $.get({
             url: 'cargarUsuario',
             dataType: "json",
@@ -232,17 +241,16 @@
                 id: id
             },
             success: function(usuario) {
-                
+
                 modificarTituloModal("Modificar usuario");
                 $("[name='ins_usuario']").val(usuario.Usuario);
                 $("[name='ins_contrasena']").val(usuario.Clave);
                 $("[name='ins_rol']").val(usuario.Rol);
                 $("[name='ins_idEmpleado']").val(usuario.IDEmpleado);
+                // nomEmpleado
+                $("#nomEmpleado").val(nombreEmpleadoPorId(usuario.IDEmpleado));               
 
                 $('#modal_add').modal('toggle');
-               
-               // nomEmpleado
-               
 
             }
         });
@@ -265,8 +273,7 @@
             success: function(data) {
                 if (data.response == "success") {
                     $('#modal_add').modal('toggle');
-                    $("#ins_idEmpleado").val('');
-                    $('#form_add').trigger("reset");
+                    limpiarModal();
 
                     Swal.fire({
                         position: 'top-end',
@@ -284,19 +291,40 @@
         });
     }
 
+    const editar = () => {
+
+        let datosFormulario = $("#form_add").serialize();
+
+        $.post({
+            url: 'agregarUsuario',
+            dataType: "json",
+            data: datosFormulario,
+            success: function(data) {
+                if (data.response == "success") {
+                    $('#modal_add').modal('toggle');
+                    limpiarModal();
+
+                    Swal.fire({
+                        position: 'top-end',
+                        icon: 'success',
+                        title: '¡Registro agregado con exito!',
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+
+                    cargarUsuarios();
+                } else {
+                    toastr["error"](data.message);
+                }
+            },
+        });
+
+    }
+
 
     (() => { //ejecutar a cargar pagina
         cargarUsuarios();
         cargarEmpleados();
-
-        /*$.get({
-            url: "cargarUsuario",
-            data: {
-                "id": 1
-            },
-            success: function(data) {
-                console.log(data);
-            }
-        });*/
+      
     })();
 </script>
